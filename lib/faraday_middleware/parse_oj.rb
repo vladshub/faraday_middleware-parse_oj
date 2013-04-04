a4d637b8075a6d32765fa733d8a020a4dbfb4e75
Vladislav Shub <vlad@yotpo.com>
2013-04-04 10:44:49 +0300

    fixed gem to work with ruby 2

--- lib/faraday_middleware/parse_oj.rb
+++ lib/faraday_middleware/parse_oj.rb
@@ -3,11 +3,11 @@ require 'faraday_middleware/response_middleware'
 module FaradayMiddleware
   class ParseOj < ResponseMiddleware
     dependency 'oj'
-    
+
     define_parser do |body|
       Oj.load(body, mode: :compat) unless body.strip.empty?
     end
-    
+
     VERSION = '0.2'
   end
 end
